d89fdfdc12e8a9c1f7aea7bc339ed511574cbd6b
EtienneZ <etiennezpro@gmail.com>
2020-07-15 15:21:46 +0200

    add modif mail

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,41 +2,36 @@ describe('Test Login Back Marcket', () =>{
 
     beforeEach('Connextion a la page LogIn', () =>{
         cy.visit('https://www.backmarket.fr/register')  
+        cy.url().should('include', '/register')
         cy.viewport('macbook-13') 
         cy.clearCookies()
         
     })
     it('Log-In with true email and password', () =>{
-        cy.get('#email-signin').type("ziegelmeyer.etienne@yahoo.fr")
-        cy.get('#password-signin').type('CybackMar#68')
+        cy.get('#email-signin').click()
+        cy.get('#email-signin').type("email@yahoo.fr")
+        cy.get('#password-signin').type('******')
         cy.get('._36HxYAE3lm-VAy2DCDaFyV').contains('Welcome Back!').click()
     })
-    it('Fail log-In', () =>{
-        cy.get('#email-signin').should('be.empty')
-        cy.get('#password-signin').should('be.empty')
+    it('Input Email and Password are empty ', () =>{
+        cy.get('#email-signin').click()
+        cy.get('#email-signin').type("email@yahoo.fr").clear({force:true})
+        cy.get('#password-signin').type('*****').clear({force:true})
         cy.get('._36HxYAE3lm-VAy2DCDaFyV').contains('Welcome Back!').click()
     })
-    it('Log-In email is empty', () =>{
-        cy.get('#email-signin').should('be.empty')
-        cy.get('#password-signin').type('CybackMar#68')
-        cy.get('._36HxYAE3lm-VAy2DCDaFyV').contains('Welcome Back!').click()
-    })
-    it('Log-In password is empty', () =>{
-        cy.get('#email-signin').type('ziegelmeyer.etienne@yahoo.fr', {
-            delay:500
-        })
-        cy.get('#password-signin').should('be.empty')
-        cy.get('._36HxYAE3lm-VAy2DCDaFyV').contains('Welcome Back!').click()
+    it('Email equal true value', () =>{
+        cy.get('#email-signin')
+            .type("email@yahoo.fr")
+            .should('have.value', 'email@yahoo.fr')
     })
     it('email contain @', () =>{
-        cy.get('#email-signin').type("ziegelmeyer.etienne@yahoo.fr")
-        cy.get('#email-signin').should('contain.value', '@')
-       
-        cy.get('#password-signin').type('CybackMar#68')
+        cy.get('#email-signin').type("email@yahoo.fr")
+        cy.get('#email-signin').should('have.value', '@')
+        cy.get('#password-signin').type('******')
         cy.get('._36HxYAE3lm-VAy2DCDaFyV').contains('Welcome Back!').click()
     })
     it('show password  ', () =>{
-        cy.get('#email-signin').type('ziegelmeyer.etienne@yahoo.fr')
+        cy.get('#email-signin').type('email@yahoo.fr')
         cy.get('[data-test=password-wrapper] > ._3qXNdv-MBFJNzill27fzzJ > [data-test=password-toggle-button]').click()
         cy.get('._36HxYAE3lm-VAy2DCDaFyV').contains('Welcome Back!').click()
     })
